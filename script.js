539ae6018767c50f3f0bc1d412f341eb802d5495
mmmm <marsitschool123@gmail.com>
2025-07-23 15:41:11 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let a = prompt('Ism krting')
+
+let b = prompt('Pasprt krting')
+
+let c = prompt('Aydi krting')
+
+
+let user ={
+        id: c,
+        ism: 'a',
+        pasprt: b,
+}
+
+
+
+
+console.log(user);
